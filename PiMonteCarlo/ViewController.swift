8c728ccc54e4e3b7e388ae3d1d0b847be5634810
Dr. Plagens <andiq@drs-mac-mini.local>
2018-04-15 13:36:36 +0530

    Initial Commit

---
+++ PiMonteCarlo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PiMonteCarlo
+//
+//  Created by Dr. Andreas Plagens on 15/04/18.
+//  Copyright © 2018 Dr. Andreas Plagens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
